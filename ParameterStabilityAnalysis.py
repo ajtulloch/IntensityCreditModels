2600a822a0d9f3378b27d30fd794a2d74a66dfa7
zaguar@gmail.com <zaguar@gmail.com>
2011-08-05 21:12:21 +1000

    Spacing

--- ParameterStabilityAnalysis.py
+++ ParameterStabilityAnalysis.py
@@ -1,37 +1,39 @@
 from CalibrationMaster import *
 import csv
 
+
 #------------------------------------------------------------------------------
 
 def GenerateCSV():
 	"""docstring for main"""
 	HP = Calibration(	DiscountCurve 	= FlatDiscountCurve(r = 0.00), 
-								CDS				= HPCreditDefaultSwap,
-								Process			= "HP",
-								Guess			= [0.01],
-								)
+						CDS				= HPCreditDefaultSwap,
+						Process			= "HP",
+						Guess			= [0.01],
+						)
 
 	CIR = Calibration(	DiscountCurve 	= FlatDiscountCurve(r = 0.00), 
-								CDS 			= CIRCreditDefaultSwap,
-								Process			= "CIR",
-								Guess			= [0.1, 0.3, 0.2, 0.02],
-								)
+						CDS 			= CIRCreditDefaultSwap,
+						Process			= "CIR",
+						Guess			= [0.1, 0.3, 0.2, 0.02],
+						)
 
 
-	IHP = InhomogenousCalibration(	DiscountCurve 	= FlatDiscountCurve(r = 0.00), 
-											)
+	IHP = InhomogenousCalibration(	
+						DiscountCurve 	= FlatDiscountCurve(r = 0.00), 
+						)
 
 	GOU = Calibration(	DiscountCurve 	= FlatDiscountCurve(r = 0.00), 
-								CDS				= GammaOUCreditDefaultSwap,
-								Process			= "GOU",
-								Guess			= [0.2, 189, 10000, 0.002],
-								)
+						CDS				= GammaOUCreditDefaultSwap,
+						Process			= "GOU",
+						Guess			= [0.2, 189, 10000, 0.002],
+						)
 
 	IGOU = Calibration(	DiscountCurve 	= FlatDiscountCurve(r = 0.00), 
-								CDS 			= IGOUCreditDefaultSwap,
-								Process			= "IGOU",
-								Guess			= [0.3, 0.8, 5, 0.02],
-								)
+						CDS 			= IGOUCreditDefaultSwap,
+						Process			= "IGOU",
+						Guess			= [0.3, 0.8, 5, 0.02],
+						)
 
 	for Calib in [GOU, IGOU]:
 
