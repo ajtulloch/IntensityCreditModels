19c208d9b126b2071c6740f657ff120576ea858a
zaguar@gmail.com <zaguar@gmail.com>
2011-08-05 17:33:05 +1000

    Revert "Removing junk, reformatting code"
    This reverts commit d950f3aebee9584753c96772100cabf723c657b2.

--- DiscountCurve.py
+++ DiscountCurve.py
@@ -1,15 +1,22 @@
 class DiscountCurveBase(object):
 	"""Defines the interface for a DiscountCurve.  The key method is DF(), 
-	which returns the discount factor from t_1 to t_2.
-	
-	To use, subclass and implement the DF() method."""
+	which returns the discount factor from t_1 to t_2.  Also defines helper 
+	methods Initialise() and ReadFile()."""
 	def __init__(self):
 		super(DiscountCurveBase, self).__init__()
 		
 	def DF(self, t1, t2):
-		"""Subclass and implement"""
+		"""docstring for DiscountFactor"""
 		abstract
+		
+	def Initialise(self, t):
+		"""docstring for Initialise"""
+		pass
 	
+	def ReadFile(self):
+		"""docstring for ReadFile"""
+		abstract
+		
 #------------------------------------------------------------------------------
 						
 class FlatDiscountCurve(DiscountCurveBase):
@@ -20,10 +27,9 @@ class FlatDiscountCurve(DiscountCurveBase):
 		
 	def DF(self, t1, t2):
 		return (1 + self.r) ** -(t2 - t1) 		
-		
+
 #------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-	x = FlatDiscountCurve(r=0.06)				
-	print x.DF(0, 10)
-
+	x = FlatDiscountCurve( r=0.06)				
+	print x.DF( 0, 10)
