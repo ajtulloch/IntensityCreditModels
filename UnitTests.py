655aca50744fe1b59857ee33155a57d82c9b3b12
zaguar@gmail.com <zaguar@gmail.com>
2011-08-05 18:23:23 +1000

    Testing unittests

---
+++ UnitTests.py
@@ -0,0 +1,67 @@
+import unittest
+
+#------------------------------------------------------------------------------
+
+class CDSTests(unittest.TestCase):
+	def setUp(self):
+		pass
+	def tearDown(self):
+		pass
+	def testDefaultSize(self):
+		pass
+	def testResize(self):
+		pass
+
+#------------------------------------------------------------------------------
+
+class CalibrationTests(unittest.TestCase):
+	"""docstring for CalibrationTests"""
+	def setUp(self):
+		pass
+	def tearDown(self):
+		pass
+	def testDefaultSize(self):
+		pass
+	def testResize(self):
+		pass
+		
+#------------------------------------------------------------------------------
+		
+class CalibrationMaster(unittest.TestCase):
+	def setUp(self):
+		pass
+	def tearDown(self):
+		pass
+	def testDefaultSize(self):
+		pass
+	def testResize(self):
+		pass
+
+#------------------------------------------------------------------------------
+
+class CreditDerivateCSVReaderTests(unittest.TestCase):
+	def setUp(self):
+		pass
+	def tearDown(self):
+		pass
+	def testDefaultSize(self):
+		pass
+	def testResize(self):
+		pass
+
+#------------------------------------------------------------------------------
+
+class MarketDataTests(unittest.TestCase):
+	def setUp(self):
+		pass
+	def tearDown(self):
+		pass
+	def testDefaultSize(self):
+		pass
+	def testResize(self):
+		pass
+
+#------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+	unittest.main()
