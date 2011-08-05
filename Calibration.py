3edcd1a382dc46ee942e67e14150a6440e1d90ed
zaguar@gmail.com <zaguar@gmail.com>
2011-08-05 18:45:24 +1000

    Refactored the RMSE function

--- Calibration.py
+++ Calibration.py
@@ -1,10 +1,12 @@
 from scipy.optimize import fmin as optimise
+from math import sqrt
+
+#------------------------------------------------------------------------------
+
 from CreditDerivativeCSVReader import *
 from CDS import *
 from DiscountCurve import *
 from MarketData import *
-from math import sqrt
-# from scipy import memoize
 
 #------------------------------------------------------------------------------
 
@@ -45,17 +47,7 @@ class Calibration(object):
 	def RMSE(self):
 		"""docstring for RM"""
 		N = len(self.MarketData.Tenors())
-		sum = 0
-		for t, market_spread in sorted(self.MarketData.Data()):
-			CDS = self.CDS(	DiscountCurve = self.DiscountCurve, 
-							maturity = t
-							)
-			model_spread = CDS.ParSpread(self.calibrated_gamma)		
-			if type(model_spread).__name__ == 'ndarray':
-				model_spread = model_spread[0]
-			sum += (model_spread - market_spread) ** 2
-		RMSE = sqrt(sum/N)
-		return RMSE	
+		return sqrt(self.ObjectiveFunction(self.calibrated_gamma) / N)
 
 	def CalibrationResults(self):
 		"""docstring for CalibrationError"""	
@@ -130,21 +122,7 @@ class InhomogenousCalibration(Calibration):
 							)
 		self.calibrated_gamma = output
 		return output
-	
-	def RMSE(self):
-		N = len(self.MarketData.Tenors())
-		sum = 0
-		for t, market_spread in sorted(self.MarketData.Data()):
-			CDS = IHPCreditDefaultSwap(	tenors = sorted(self.MarketData.Tenors()), 
-										DiscountCurve = self.DiscountCurve, 
-										maturity = t
-										)
-			model_spread = CDS.ParSpread(self.calibrated_gamma)	
-			sum += (model_spread - market_spread) ** 2
 		
-		RMSE = sqrt(sum/N)
-		return RMSE
-	
 	def CalibrationResults(self):
 		"""docstring for CalibrationError"""
 		print "-" * 80
