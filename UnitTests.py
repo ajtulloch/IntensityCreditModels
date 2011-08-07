acb848ef487528e9d2701b48d47c058c118b3a1f
zaguar@gmail.com <zaguar@gmail.com>
2011-08-07 12:37:45 +1000

    Added tests for MarketData

--- UnitTests.py
+++ UnitTests.py
@@ -1,65 +1,41 @@
 import unittest
+import MarketData
 
 #------------------------------------------------------------------------------
 
 class CDSTests(unittest.TestCase):
-	def setUp(self):
-		pass
-	def tearDown(self):
-		pass
-	def testDefaultSize(self):
-		pass
-	def testResize(self):
-		pass
-
+	pass
 #------------------------------------------------------------------------------
 
 class CalibrationTests(unittest.TestCase):
-	"""docstring for CalibrationTests"""
-	def setUp(self):
-		pass
-	def tearDown(self):
-		pass
-	def testDefaultSize(self):
-		pass
-	def testResize(self):
-		pass
-		
+	pass
 #------------------------------------------------------------------------------
 		
 class CalibrationMaster(unittest.TestCase):
-	def setUp(self):
-		pass
-	def tearDown(self):
-		pass
-	def testDefaultSize(self):
-		pass
-	def testResize(self):
-		pass
-
+	pass
 #------------------------------------------------------------------------------
 
 class CreditDerivateCSVReaderTests(unittest.TestCase):
-	def setUp(self):
-		pass
-	def tearDown(self):
-		pass
-	def testDefaultSize(self):
-		pass
-	def testResize(self):
-		pass
-
+	pass
 #------------------------------------------------------------------------------
 
 class MarketDataTests(unittest.TestCase):
 	def setUp(self):
-		pass
+		spreads = {'Date' : '17/5/10', '1' : '200', '2' : '250' }
+		self.mkt_data = MarketData.MarketData(spreads)
 	def tearDown(self):
 		pass
-	def testDefaultSize(self):
-		pass
-	def testResize(self):
-		pass
+	def testDate(self):
+		date = self.mkt_data.Date()
+		self.assertEqual(date, '17/5/10')
+	def testTenors(self):
+		tenors = self.mkt_data.Tenors()
+		self.assertEqual(tenors, [1, 2])
+		
+	def testData(self):
+		data = self.mkt_data.Data()
+		self.assertEqual(data, [(1, 200), (2, 250)])
+		
 
 #------------------------------------------------------------------------------
 
